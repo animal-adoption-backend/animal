4b074c8aaa98f67097c9b79a24d7c754bb4c4e8b
onanalee <69330162+onanalee@users.noreply.github.com>
2021-07-17 07:20:20 +0900

    committing before pushing to origin main

---
+++ schemas/animals.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const animalsSchema = new Schema({
+    animalId:{
+        type:Number,
+        required:true,
+    },
+    userId: {
+        type: Number,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    animalName: {
+        type: String,
+        required: true,
+    },
+    animalSpecies: {
+        type: String,
+        required: true,
+    },
+    animalBreed: {
+        type: String,
+        required: true,
+    },
+    animalGender: {
+        type: String,
+        required: true,
+    },
+    animalAge: {
+        type: Number,
+        required: true,
+    },
+    animalStory: {
+        type: String,
+        required: true,
+    },
+    animalPhoto: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("posts",postsSchema);
